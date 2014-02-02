3bb25bc2b46554232b2eae54d65022b3c134fe66
FirstSpring <firstspring1845@gmail.com>
2014-02-02 23:56:59 +0900

    リプライの判定方法を変更

--- Javatter_for_PC/src/com/orekyuu/javatter/controller/UserStreamController.java
+++ Javatter_for_PC/src/com/orekyuu/javatter/controller/UserStreamController.java
@@ -2,37 +2,39 @@ package com.orekyuu.javatter.controller;
 
 import twitter4j.Status;
 import twitter4j.TwitterException;
+import twitter4j.UserMentionEntity;
 import twitter4j.UserStreamAdapter;
 
 import com.orekyuu.javatter.account.AccountManager;
-import com.orekyuu.javatter.account.TwitterManager;
 import com.orekyuu.javatter.logic.UserStreamLogic;
 
 /**
  * ユーザーストリームのController
+ * 
  * @author orekyuu
- *
+ * 
  */
-public class UserStreamController extends UserStreamAdapter{
+public class UserStreamController extends UserStreamAdapter {
 
 	private UserStreamLogic model;
 
 	/**
 	 * モデルを設定
+	 * 
 	 * @param model
 	 */
-	public void setModel(UserStreamLogic model){
-		this.model=model;
+	public void setModel(UserStreamLogic model) {
+		this.model = model;
 	}
 
 	@Override
-	public void onStatus(Status status){
+	public void onStatus(Status status) {
 		model.onStatus(status);
 		try {
-			if(isReply(status)){
+			if (isReply(status)) {
 				model.onReplyTweet(status);
 			}
-			if(status.isRetweet()){
+			if (status.isRetweet()) {
 				model.onRetweetTweet(status);
 			}
 		} catch (IllegalStateException e) {
@@ -42,8 +44,17 @@ public class UserStreamController extends UserStreamAdapter{
 		}
 	}
 
-	private boolean isReply(Status status) throws IllegalStateException, TwitterException{
-		String user=AccountManager.getInstance().getAccessToken().getScreenName();
-		return user.equals(status.getInReplyToScreenName());
+	private boolean isReply(Status status) throws IllegalStateException,
+			TwitterException {
+		if (status == null) {
+			return false;
+		}
+		for (UserMentionEntity ume : status.getUserMentionEntities()) {
+			if (ume.getScreenName().equals(
+					AccountManager.getInstance().getAccessToken().getScreenName())) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
